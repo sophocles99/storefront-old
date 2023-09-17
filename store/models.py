d8824e38584e4f4627958551150e37b879abf24f
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-09-17 22:03:11 +0100

    add featured_product field - circular dependency

--- store/models.py
+++ store/models.py
@@ -5,8 +5,10 @@ class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
 
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
 
 
 class Product(models.Model):
